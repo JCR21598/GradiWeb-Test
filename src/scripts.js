9b0778b6ba0cf9376829fc4bf4cf3fe2e421dc9b
JCR21598 <46407474+JCR21598@users.noreply.github.com>
2021-02-19 11:19:34 -0500

    Add a bunch to repo

--- src/scripts.js
+++ src/scripts.js
@@ -1,30 +1,35 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+
+    const key_api = d29cf117a0fb58b32079849c93c64387;
+
+    // Get Bogota's current weather
+    bogotaWeatherData = bogotaWeather(key_api)
+
+    // Get Bogota's 3 Day weather forecast
+    bogotaForecastWeather = bogotaForecast(key_api)
+
+    // Get the weather from place user input's
+
+
+    // For Bogota's Today and 3 day forecast add it when page loads
+    
 });
 
 
-
-// // When the webpage loads
-// document.addEventListener('DOMContentLoaded', function() {
-    
-//     const key_api = d29cf117a0fb58b32079849c93c64387;
-
-//     // Get current weather
-
-
-    
-//     func();
-
-
-
-
-
-
-// }, false);
-
-
-// function func(){
+function bogotaWeather(){
 
     
 
-// }
+}
+
+function bogotaForecast(){
+
+
+
+}
+
+function addDataOnPageLoad(){
+
+    
+
+}
