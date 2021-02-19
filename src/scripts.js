81d2c35754d93f66f7be6d61864387e4bb33c4f5
JCR21598 <46407474+JCR21598@users.noreply.github.com>
2021-02-19 11:12:06 -0500

    jquery test

--- src/scripts.js
+++ src/scripts.js
@@ -1,19 +1,30 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
 
-// When the webpage loads
-document.addEventListener('DOMContentLoaded', function() {
+
+
+// // When the webpage loads
+// document.addEventListener('DOMContentLoaded', function() {
     
-    func();
+//     const key_api = d29cf117a0fb58b32079849c93c64387;
+
+//     // Get current weather
+
+
+    
+//     func();
 
 
 
 
 
 
-}, false);
+// }, false);
 
 
-function func(){
+// function func(){
 
-    api.openweathermap.org/data/2.5/find?q=Bogota&units=metric
+    
 
-}
+// }
