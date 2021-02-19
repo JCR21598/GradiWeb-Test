875602cb0fa0f6496f1d1ffa3a785be4aa7888bf
JCR21598 <46407474+JCR21598@users.noreply.github.com>
2021-02-19 10:14:24 -0500

    Header, card and places to visit

---
+++ src/scripts.js
@@ -0,0 +1,19 @@
+
+// When the webpage loads
+document.addEventListener('DOMContentLoaded', function() {
+    
+    func();
+
+
+
+
+
+
+}, false);
+
+
+function func(){
+
+    api.openweathermap.org/data/2.5/find?q=Bogota&units=metric
+
+}
