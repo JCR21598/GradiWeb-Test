2abe30399b053edb04c5a983ca1ea0ecdf5f034a
JCR21598 <46407474+JCR21598@users.noreply.github.com>
2021-02-19 21:26:33 -0500

    bogota forecast almost done

--- src/scripts.js
+++ src/scripts.js
@@ -1,14 +1,12 @@
 $( document ).ready(function() {
 
-    alert("hi")
-
     const api_key = "d29cf117a0fb58b32079849c93c64387";
 
     // Get Bogota's current weather and set to corresponding section
     bogotaWeatherData = bogotaWeather(api_key, "Bogota");
 
     // Get Bogota's 3 Day weather forecast
-    bogotaForecastWeather = bogotaForecast(api_key);
+    bogotaForecastWeather = bogotaForecast(api_key, 4.71, -74.08, "current,minutely,hourly,alerts" );
 
     // Get the weather from place user input's
 
@@ -25,15 +23,10 @@ function bogotaWeather(api_key, city_name){
     // Make API call and fill in elements
     $.getJSON(api_call, function(data) {
 
-        bogotaIconId = data["weather"][0]["icon"];
-        bogotaTemp = data["main"]["temp"];
+        var bogotaIconId = data["weather"][0]["icon"];
+        var bogotaTemp = data["main"]["temp"];
         
-        bogotaIcon = `http://openweathermap.org/img/w/${bogotaIconId}.png`
-        
-        console.log(JSON.stringify(data))
-
-        console.log(`Bogota - Weather Icon ID: ${bogotaIcon}`)
-        console.log(`Bogota - Weather Temp: ${bogotaTemp}`)
+        var bogotaIcon = `https://openweathermap.org/img/w/${bogotaIconId}.png`
 
         var celsius = '&#8451'.sup();
 
@@ -43,10 +36,80 @@ function bogotaWeather(api_key, city_name){
       })
 }
 
-function bogotaForecast(){
+function bogotaForecast(api_key, lat, lon, exclude){
+
+    var dayReference = {
+        0: "Monday",
+        1: "Tuesday",
+        2: "Wednesday",
+        3: "Thursday",
+        4: "Friday",
+        5: "Saturday",
+        6: "Sunday", 
+    };
+
+    var celsius = '&#8451';
+
+    var api_call = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=${exclude}&units=metric&appid=${api_key}`;
+
+     // Make API call and fill in elements
+     $.getJSON(api_call, function(data) {
+
+        console.log(JSON.stringify(data));
+
+        for (count = 0; count < 3; count++) {
+
+            // Extract and transform and values necessary
+            var unix_timestamp = data["daily"][count]["dt"]
+            var date = new Date(unix_timestamp * 1000);
+            var day = date.getDay();
+            var dayName = dayReference[day]
+
+            var dayMinTemp = data["daily"][count]["temp"]["min"];
+            var dayMaxTemp = data["daily"][count]["temp"]["max"];
+            var tempsOfDay = `${Math.round(dayMinTemp)} / ${Math.round(dayMaxTemp)}`;
+
+            var dayWeatherInfo = data["daily"][count]["weather"][0]["main"];
+
+            var dayWeatherIconId = data["daily"][count]["weather"][0]["icon"];
+            var dayWeatherIcon = `https://openweathermap.org/img/w/${dayWeatherIconId}.png`
 
 
+            // Create elements for parent div
+            var dayWeatherIconElem = document.createElement("img");
+            var dayElem = document.createElement("div");
+            var weatherInfoElem = document.createElement("div");
+            var tempElem = document.createElement("div");
 
+            console.log(dayWeatherIconElem);
+
+            // Set class attr for styling
+            dayWeatherIconElem.setAttribute("class", "bogota-weather-icon");
+            dayElem.setAttribute("class", "bogota-day");
+            weatherInfoElem.setAttribute("class", "bogota-weather-info");
+            tempElem.setAttribute("class", "bogota-temp");
+
+            // Add values to newly created elements
+            var respDay = document.createTextNode(dayName);
+            var respWeatherInfo = document.createTextNode(dayWeatherInfo);
+            var respTemp = document.createTextNode(tempsOfDay);
+
+            // Combine answer with element
+            dayWeatherIconElem.setAttribute("src", dayWeatherIcon);
+            dayElem.appendChild(respDay);
+            weatherInfoElem.appendChild(respWeatherInfo);
+            tempElem.appendChild(respTemp);
+
+            // Append created elements to parent div 
+            $(`.forecast-day${count + 1}`).append(dayWeatherIconElem, dayElem, weatherInfoElem, tempElem); 
+          }
+        
+        
+
+
+ 
+      })
+    
 }
 
 function addDataOnPageLoad(){
